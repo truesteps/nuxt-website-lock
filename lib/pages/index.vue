c8463100c87ee03b86825f9664e9d0263fdc5234
Patrik  Jánosdeák <patrik.janosdeak@bistroagency.cz>
2020-10-30 12:41:06 +0100

    refactor: update styles to be scoped under website-lock class + bump version for npm

--- lib/pages/index.vue
+++ lib/pages/index.vue
@@ -1,34 +1,36 @@
 <template>
-  <div class="login">
-    <div v-if="errors.length > 0" class="alert__wrap">
-      <div v-for="(error, index) in errors" :key="index" class="login__form-error">
-        {{ error }}
+  <div class="website-lock">
+    <div class="login">
+      <div v-if="errors.length > 0" class="alert__wrap">
+        <div v-for="(error, index) in errors" :key="index" class="login__form-error">
+          {{ error }}
+        </div>
       </div>
+
+      <h1 class="login__heading">
+        This website is locked.
+      </h1>
+
+      <form class="login__form" method="post" action="#" @submit.prevent="sendForm">
+        <div class="login__form-input-wrap">
+          <label for="login-password" class="login__form-label">Password</label>
+
+          <input
+            id="login-password"
+            v-model="password"
+            class="login__form-password"
+            type="password"
+            name="website-lock-password"
+            placeholder="Insert password..."
+            autofocus
+          >
+
+          <button type="submit" class="login__form-submit">
+            Unlock
+          </button>
+        </div>
+      </form>
     </div>
-
-    <h1 class="login__heading">
-      This website is locked.
-    </h1>
-
-    <form class="login__form" method="post" action="#" @submit.prevent="sendForm">
-      <div class="login__form-input-wrap">
-        <label for="login-password" class="login__form-label">Password</label>
-
-        <input
-          id="login-password"
-          v-model="password"
-          class="login__form-password"
-          type="password"
-          name="website-lock-password"
-          placeholder="Insert password..."
-          autofocus
-        >
-
-        <button type="submit" class="login__form-submit">
-          Unlock
-        </button>
-      </div>
-    </form>
   </div>
 </template>
 
@@ -91,118 +93,111 @@
   body {
     padding: 15px;
     margin: 0;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-    -ms-flex-direction: column;
-    flex-direction: column;
-    font-family: "HelveticaNeueLT-Bold", Helvetica, Arial, sans-serif;
-  }
-
-  body, html {
     background-color: #f74902;
-    width: 100%;
-    height: 100%;
   }
 
-  .login {
-    position: relative;
-    width: 100%;
-    max-width: 320px;
-  }
+  .website-lock {
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: calc(100vw - 30px);
+    height: calc(100vh - 30px);
 
-  .login__heading {
-    font-size: 22px;
-    color: #ffffff;
-    margin-top: 0;
-    margin-bottom: 1em;
-    font-weight: 500;
-  }
-
-  .login__form {
-    margin-bottom: 26px;
-
-    &-error {
-      background: #da0000;
-      border-radius: 3px;
-      color: #fff;
-      font-size: 14px;
-      padding: 10px;
-      margin: 0;
-      margin-bottom: 5px;
+    .login {
+      position: relative;
+      width: 100%;
+      max-width: 320px;
+      font-family: "HelveticaNeueLT-Bold", Helvetica, Arial, sans-serif;
     }
 
-    &-label {
-      display: none;
-    }
-
-    &-input-wrap {
-      display: -webkit-box;
-      display: -ms-flexbox;
-      display: flex;
-    }
-
-    &-password {
-      background-color: #eaeaff;
-      -moz-appearance: none;
-      -webkit-appearance: none;
-      padding: 5px 10px;
-      margin: 0;
-      height: 36px;
-      font-size: 14px;
-      border: 0;
-      border-radius: 0;
-      border-top-left-radius: 3px;
-      border-bottom-left-radius: 3px;
-      min-width: 0;
-      -webkit-box-flex: 1;
-      -ms-flex: 1 1 0px;
-      flex: 1 1 0;
-      -webkit-transition: background-color 200ms ease;
-      -o-transition: background-color 200ms ease;
-      transition: background-color 200ms ease;
-    }
-
-    &-password:focus,
-    &-password:hover {
-      outline: 0;
-      background-color: #ffffff;
-    }
-
-    &-submit {
-      -moz-appearance: none;
-      -webkit-appearance: none;
-      border: 0;
-      border-radius: 0;
-      border-top-right-radius: 3px;
-      border-bottom-right-radius: 3px;
-      background-color: #232323;
+    .login__heading {
+      font-size: 22px;
       color: #ffffff;
-      margin: 0;
-      padding: 10px;
-      height: 36px;
-      -webkit-transition: background-color 200ms ease;
-      -o-transition: background-color 200ms ease;
-      transition: background-color 200ms ease;
-      font-size: 14px;
-      -webkit-box-flex: 0;
-      -ms-flex: 0 0 auto;
-      flex: 0 0 auto;
+      margin-top: 0;
+      margin-bottom: 1em;
+      font-weight: 500;
     }
 
-    &-submit:focus,
-    &-submit:hover {
-      outline: 0;
-      cursor: pointer;
-      background: #101010;
+    .login__form {
+      margin-bottom: 26px;
+
+      &-error {
+        background: #da0000;
+        border-radius: 3px;
+        color: #fff;
+        font-size: 14px;
+        padding: 10px;
+        margin: 0;
+        margin-bottom: 5px;
+      }
+
+      &-label {
+        display: none;
+      }
+
+      &-input-wrap {
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+      }
+
+      &-password {
+        background-color: #eaeaff;
+        -moz-appearance: none;
+        -webkit-appearance: none;
+        padding: 5px 10px;
+        margin: 0;
+        height: 36px;
+        font-size: 14px;
+        border: 0;
+        border-radius: 0;
+        border-top-left-radius: 3px;
+        border-bottom-left-radius: 3px;
+        min-width: 0;
+        -webkit-box-flex: 1;
+        -ms-flex: 1 1 0px;
+        flex: 1 1 0;
+        -webkit-transition: background-color 200ms ease;
+        -o-transition: background-color 200ms ease;
+        transition: background-color 200ms ease;
+      }
+
+      &-password:focus,
+      &-password:hover {
+        outline: 0;
+        background-color: #ffffff;
+      }
+
+      &-submit {
+        -moz-appearance: none;
+        -webkit-appearance: none;
+        border: 0;
+        border-radius: 0;
+        border-top-right-radius: 3px;
+        border-bottom-right-radius: 3px;
+        background-color: #232323;
+        color: #ffffff;
+        margin: 0;
+        padding: 10px;
+        height: 36px;
+        -webkit-transition: background-color 200ms ease;
+        -o-transition: background-color 200ms ease;
+        transition: background-color 200ms ease;
+        font-size: 14px;
+        -webkit-box-flex: 0;
+        -ms-flex: 0 0 auto;
+        flex: 0 0 auto;
+      }
+
+      &-submit:focus,
+      &-submit:hover {
+        outline: 0;
+        cursor: pointer;
+        background: #101010;
+      }
     }
   }
 </style>
